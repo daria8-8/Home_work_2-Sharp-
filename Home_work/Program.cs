7995c98b3f47baee8c99a9e4de12a5f0e01eaa47
Daria <daryaneronova@yandex.ru>
2022-09-21 12:41:51 +0300

    Home_work

---
+++ Home_work/Program.cs
@@ -0,0 +1,37 @@
+﻿void Ex10()
+{
+Console.WriteLine("Введите трёхзначное число : ");
+int a = int.Parse(Console.ReadLine());
+int result = ((a / 10) % 10);
+Console.Write(result);
+}
+
+void Ex13()
+{
+Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine());
+if (a < 99) //if (a / 100 == 0)
+    {
+        Console.Write("Третьей цифры нет");
+    }
+else
+    {
+        Console.Write(a.ToString()[2]);
+    }
+}
+
+void Ex15()
+{
+Console.WriteLine("Введите цифру обозначающую день недели, от 1 до 7 : ");
+int a = int.Parse(Console.ReadLine());
+if (a < 6)
+    {
+        Console.Write("Нет, это будний день");
+    }
+else
+    {
+        Console.Write("Да, день выходной");
+    }
+}
+
+Ex15();
